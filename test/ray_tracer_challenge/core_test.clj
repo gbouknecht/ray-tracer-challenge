7e3abc71723b05966c5209840f8c96cf52104cdd
Gerrit N. Bouknecht <gbouknecht@server.fake>
2022-10-04 18:54:58 +0200

    Initial commit

---
+++ test/ray_tracer_challenge/core_test.clj
@@ -0,0 +1,7 @@
+(ns ray-tracer-challenge.core-test
+  (:require [clojure.test :refer :all]
+            [ray-tracer-challenge.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
