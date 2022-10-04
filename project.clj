af2b74d81ebb999d8eeeed0eaa96b4c2abbbb6bc
Gerrit N. Bouknecht <gbouknecht@server.fake>
2022-10-04 20:11:22 +0200

    Configure src as test path
    Instead of putting the (unit) test files in a separate test directory I choose
    to put the test file next to the file (namespace) it is testing. By doing this
    we do not have to duplicate the src directory structure into the test directory
    anymore. Furthermore, I think it is handy to have the tests nearby. And if you
    see these tests as a kind of specification (by example) of your code, why not
    put them together.

--- project.clj
+++ project.clj
@@ -1,10 +1,17 @@
+(def ^:private test-file-pattern #".*_test.clj")
+
 (defproject ray-tracer-challenge "0.1.0-SNAPSHOT"
   :description "The Ray Tracer Challenge"
   :url "http://server.fake/ray-tracer-challenge"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :main ^:skip-aot ray-tracer-challenge.core
+  :test-paths ["src"]
+  :test-selectors {:default [(fn [ns] (.endsWith (str ns) "-test"))
+                             (constantly true)]}
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+  :profiles {:uberjar {:aot      :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
+  :jar-exclusions ~[test-file-pattern]
+  :uberjar-exclusions ~[test-file-pattern])
