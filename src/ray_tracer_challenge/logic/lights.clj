e5a7fcec3ad1d864ac1067dbd006150db4c53259
Gerrit N. Bouknecht <gbouknecht@server.fake>
2022-11-05 14:54:09 +0100

    Add lighting function

--- src/ray_tracer_challenge/logic/lights.clj
+++ src/ray_tracer_challenge/logic/lights.clj
@@ -1,3 +1,23 @@
-(ns ray-tracer-challenge.logic.lights)
+(ns ray-tracer-challenge.logic.lights
+  (:require [clojure.math.numeric-tower :refer [expt]]
+            [ray-tracer-challenge.logic.colors :refer :all]
+            [ray-tracer-challenge.logic.tuples :refer :all]))
 
 (defn point-light [position intensity] {:position position :intensity intensity})
+(defn lighting [material light point eye-vektor normal-vektor]
+  (let [effective-color (multiply-colors (:color material) (:intensity light))
+        light-vektor (normalize-vektor (subtract-tuples (:position light) point))
+        ambient-color (multiply-color effective-color (:ambient material))
+        light-dot-normal (dot-product-tuples light-vektor normal-vektor)
+        diffuse-color (if (< light-dot-normal 0)
+                        (color 0 0 0)
+                        (reduce multiply-color [effective-color (:diffuse material) light-dot-normal]))
+        specular-color (if (< light-dot-normal 0)
+                         (color 0 0 0)
+                         (let [reflect-vektor (reflect (negate-tuple light-vektor) normal-vektor)
+                               reflect-dot-eye (dot-product-tuples reflect-vektor eye-vektor)]
+                           (if (<= reflect-dot-eye 0)
+                             (color 0 0 0)
+                             (let [factor (expt reflect-dot-eye (:shininess material))]
+                               (reduce multiply-color [(:intensity light) (:specular material) factor])))))]
+    (reduce add-colors [ambient-color diffuse-color specular-color])))
--- src/ray_tracer_challenge/logic/lights_test.clj
+++ src/ray_tracer_challenge/logic/lights_test.clj
@@ -1,8 +1,11 @@
 (ns ray-tracer-challenge.logic.lights-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.math.numeric-tower :refer [sqrt]]
+            [clojure.test :refer :all]
             [ray-tracer-challenge.logic.colors :refer :all]
             [ray-tracer-challenge.logic.lights :refer :all]
-            [ray-tracer-challenge.logic.tuples :refer :all]))
+            [ray-tracer-challenge.logic.materials :refer :all]
+            [ray-tracer-challenge.logic.tuples :refer :all]
+            [ray-tracer-challenge.test.test-utils :refer :all]))
 
 (deftest about-point-lights
 
@@ -12,3 +15,43 @@
           light (point-light position intensity)]
       (is (= position (:position light)))
       (is (= intensity (:intensity light))))))
+
+(deftest about-lighting
+
+  (let [material (material)
+        position (point 0 0 0)]
+
+    (testing "should be able to handle eye between light and surface"
+      (let [eye-vektor (vektor 0 0 -1)
+            normal-vektor (vektor 0 0 -1)
+            light (point-light (point 0 0 -10) (color 1 1 1))
+            result (lighting material light position eye-vektor normal-vektor)]
+        (is (roughly (color 1.9 1.9 1.9) result))))
+
+    (testing "should be able to handle eye between light and surface, eye offset 45°"
+      (let [eye-vektor (vektor 0 (/ (sqrt 2) 2) (- (/ (sqrt 2) 2)))
+            normal-vektor (vektor 0 0 -1)
+            light (point-light (point 0 0 -10) (color 1 1 1))
+            result (lighting material light position eye-vektor normal-vektor)]
+        (is (roughly (color 1 1 1) result))))
+
+    (testing "should be able to handle eye opposite surface, light offset 45°"
+      (let [eye-vektor (vektor 0 0 -1)
+            normal-vektor (vektor 0 0 -1)
+            light (point-light (point 0 10 -10) (color 1 1 1))
+            result (lighting material light position eye-vektor normal-vektor)]
+        (is (roughly (color 0.7364 0.7364 0.7364) result))))
+
+    (testing "should be able to handle eye in path of reflection vector"
+      (let [eye-vektor (vektor 0 (- (/ (sqrt 2) 2)) (- (/ (sqrt 2) 2)))
+            normal-vektor (vektor 0 0 -1)
+            light (point-light (point 0 10 -10) (color 1 1 1))
+            result (lighting material light position eye-vektor normal-vektor)]
+        (is (roughly (color 1.6364 1.6364 1.6364) result))))
+
+    (testing "should be able to handle light behind surface"
+      (let [eye-vektor (vektor 0 0 -1)
+            normal-vektor (vektor 0 0 -1)
+            light (point-light (point 0 0 10) (color 1 1 1))
+            result (lighting material light position eye-vektor normal-vektor)]
+        (is (roughly (color 0.1 0.1 0.1) result))))))
